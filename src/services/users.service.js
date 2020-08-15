48ffd433482c8dcb8e8e78c2c2663dcb2aac170e
Amit Maurya <amit.maurya@ltts.com>
2020-08-16 02:13:03 +0530

    enviorument url changes

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,55 +1,51 @@
-import axios from 'axios';
+import axios from "axios";
 const BASE_URL = "https://jsonplaceholder.typicode.com/";
 
 export const getAllUsers = () => {
-
-    return axios(`${process.env.REACT_APP_TEST_VAR}users`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-        }
-    })
-        .then(response => response.data)
-        .catch(error => {
-            throw error;
-        });
+  return axios(`${BASE_URL}users`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
 export const getUserData = (id) => {
-
-    return axios(`${process.env.REACT_APP_TEST_VAR}users/${id}`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-        }
-    })
-        .then(response => response.data)
-        .catch(error => {
-            throw error;
-        });
+  return axios(`${BASE_URL}users/${id}`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
 export const getUsersPost = (id) => {
-
-    return axios(`${process.env.REACT_APP_TEST_VAR}posts?userId=${id}`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-        }
-    })
-        .then(response => response.data)
-        .catch(error => {
-            throw error;
-        });
+  return axios(`${BASE_URL}posts?userId=${id}`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
 export const getUsersComments = (id) => {
-
-    return axios(`${process.env.REACT_APP_TEST_VAR}comments?postId=${id}`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-        }
-    })
-        .then(response => response.data)
-        .catch(error => {
-            throw error;
-        });
+  return axios(`${BASE_URL}comments?postId=${id}`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
